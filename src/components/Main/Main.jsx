29760beffe01c2ff04410cdf000c023a8adca108
OlehKotov <mail.oleg456@gmail.com>
2024-09-26 22:11:12 +0300

    Update Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,10 @@ const Main = () => {
     <div className={css.home}>
       <Header />
       <main>
-        <div className={css.blockWithText}></div>
+        <div className={css.blockWithText}>
+          <h1 className={css.blockHeader}>Take good <span>care</span> of your small pets</h1>
+          <p className={css.blockText}>Choosing a pet for your home is a choice that is meant to enrich your life with immeasurable joy and tenderness.</p>
+        </div>
         <div className={css.blockWithImg}></div>
       </main>
     </div>
